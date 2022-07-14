a933e670f2a3c2909834fd89cbcf370ecb262ed5
Michael <michaelmkrtch@gmail.com>
2022-07-13 19:19:47 -0700

    Add game rule logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
+let gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
 
 const players = [
   {
@@ -15,10 +24,17 @@ const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorOutputElement = document.getElementById('config-errors');
+const missingNameOverlay = document.getElementById('no-name-overlay');
+const gameField = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayer1Btn = document.getElementById('edit-player1-btn');
 const editPlayer2Btn = document.getElementById('edit-player2-btn');
 const cancelConfigBtn = document.getElementById('cancel-config-btn');
+const startNewGameBtn = document.getElementById('start-game-btn');
+const continueOverlayBtn = document.getElementById('continue-btn');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1Btn.addEventListener('click', openPlayerConfig);
 editPlayer2Btn.addEventListener('click', openPlayerConfig);
@@ -27,3 +43,9 @@ cancelConfigBtn.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtn.addEventListener('click', startNewGame);
+
+continueOverlayBtn.addEventListener('click', closeWarningOverlay);
+
+gameBoardElement.addEventListener('click', selectGameField);
